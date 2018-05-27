c325946f40077024a6850754917d83d2bdeb6e15
Ryan Fleck <ryan.fleck@protonmail.com>
2018-05-27 12:21:27 -0400

    Attempted to write id-matching rule in overtime test.

--- test-runs/qc-gas-tax/compile-run.sh
+++
@@ -1,7 +0,0 @@
-#Compile and Run - QC Gas Tax
-cd ~/xalgo/xa-rules
-bundle exec ruby cli.rb ~/xalgo/rules-interpreter/test-runs/qc-gas-tax/proximity-gas-tax.rule ~/xalgo/rules-interpreter/test-runs/qc-gas-tax/proximity-gas-tax.rule.json
-echo "[info] [cnr] Compiled. Running..."
-cd ~/xalgo/rules-interpreter
-sbt "runMain org.xalgorithms.rules.Runner test-runs/qc-gas-tax/"
-echo "[info] [cnr] Done."
