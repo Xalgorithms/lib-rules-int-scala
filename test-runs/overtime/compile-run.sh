39f54070369bdabe3db55afe8e242d2f00a4fabb
Ryan Fleck <ryan.fleck@protonmail.com>
2018-05-27 11:37:18 -0400

    Fix compile/run scripts, fix algebra.rule.

--- test-runs/overtime/compile-run.sh
+++
@@ -1,8 +0,0 @@
-#Compile and Run - Overtime
-rm ~/xalgo/rules-interpreter/test-runs/overtime/overtime.rule.json
-cd ~/xalgo/xa-rules
-bundle exec ruby cli.rb ~/xalgo/rules-interpreter/test-runs/overtime/overtime.rule ~/xalgo/rules-interpreter/test-runs/overtime/overtime.rule.json
-echo "[info] [cnr] Compiled. Running..."
-cd ~/xalgo/rules-interpreter
-sbt "runMain org.xalgorithms.rules.Runner test-runs/overtime/"
-echo "[info] [cnr] Done."
